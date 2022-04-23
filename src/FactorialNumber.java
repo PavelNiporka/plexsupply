855075a1c37879dcc58e7499ccdf6e61a3e88a0c
Pasha <pavelniporka@gmail.com>
2022-04-23 18:21:51 +0300

    Initial commit

---
+++ src/FactorialNumber.java
@@ -0,0 +1,34 @@
+import com.google.common.math.BigIntegerMath;
+
+import java.io.*;
+import java.math.BigInteger;
+
+
+public class FactorialNumber {
+
+    public static void main(String[] args) throws IOException {
+
+        File input = new File("C:\\Users\\Pasha\\Downloads\\input.txt");
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(input));
+        File output = new File("C:\\Users\\Pasha\\Downloads\\output.txt");
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(output));
+        String st;
+        int num = 0;
+        while ((st = bufferedReader.readLine()) != null) {
+            try {
+                num = Integer.parseInt(st);
+                bufferedWriter.write(st + " = " + factorial(num));
+                bufferedWriter.newLine();
+            } catch (NumberFormatException | IOException nfe) {
+                // Handle the condition when str is not a number.
+            }
+
+        }
+        bufferedReader.close();
+
+    }
+    public static BigInteger factorial(int n) {
+        return BigIntegerMath.factorial(n);
+    }
+
+}
